a9423c0c40d5f3366cddf6388cb7270e21b41edf
imtiazahammed <ahammedrifat1920@gmail.com>
2025-05-19 00:02:05 +0600

    check 2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/// check git push
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
